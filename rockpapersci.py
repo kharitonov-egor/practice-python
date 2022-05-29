3e8b27d23e1e8ec2e86c407487db535f340dd3fe
Egor Kharitonov <ega.khar@gmail.com>
2022-05-29 01:11:52 -0400

    check human2

--- rockpapersci.py
+++ rockpapersci.py
@@ -7,8 +7,7 @@ computer_choice = random.choice(list_moves)
 human_choice = input("Please input rock, paper or scissors: ")
 
 if human_choice not in list_moves:
-    print("fuck!")
-
+    print("Incorrect choise, my friend!")
 
 if computer_choice == human_choice:
     print("You have a tie!")
@@ -16,4 +15,13 @@ elif computer_choice == "rock" and human_choice == "paper":
     print("You win!")
 elif computer_choice == "rock" and human_choice == "scissors":
     print("You lose!")
+elif computer_choice == "paper" and human_choice == "scissors":
+    print("You win!")
+elif computer_choice == "paper" and human_choice == "rock":
+    print("You lose!")
+elif computer_choice == "scissors" and human_choice == "rock":
+    print("You win!")
+elif computer_choice == "scissors" and human_choice == "paper":
+    print("You lose!")
+
 
