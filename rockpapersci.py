2fe01f985857c9eafee57a2aceba23871c63ce42
Egor Kharitonov <ega.khar@gmail.com>
2022-05-29 01:29:35 -0400

    changing variables

--- rockpapersci.py
+++ rockpapersci.py
@@ -1,10 +1,10 @@
 import random
 
-list_moves = ("rock","paper","scissors")
+list_moves = ("r","p","s")
 
 computer_choice = random.choice(list_moves)
 
-human_choice = input("Please input rock, paper or scissors: ")
+human_choice = input("Please input r, p or s: ")
 
 if human_choice not in list_moves:
     print("Incorrect choise, my friend!")
@@ -13,18 +13,20 @@ print(f"Thanks for picking {human_choice}!")
 print(f"Computer has picked {computer_choice}!")
 
 if computer_choice == human_choice:
+
     print("You have a tie!")
-elif computer_choice == "rock" and human_choice == "paper":
+
+elif computer_choice == "r" and human_choice == "p":
     print("You win!")
-elif computer_choice == "rock" and human_choice == "scissors":
+elif computer_choice == "r" and human_choice == "s":
     print("You lose!")
-elif computer_choice == "paper" and human_choice == "scissors":
+elif computer_choice == "p" and human_choice == "s":
     print("You win!")
-elif computer_choice == "paper" and human_choice == "rock":
+elif computer_choice == "p" and human_choice == "r":
     print("You lose!")
-elif computer_choice == "scissors" and human_choice == "rock":
+elif computer_choice == "s" and human_choice == "r":
     print("You win!")
-elif computer_choice == "scissors" and human_choice == "paper":
+elif computer_choice == "s" and human_choice == "p":
     print("You lose!")
 
 
