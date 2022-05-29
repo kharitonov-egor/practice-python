3b31c80b1b39b1dbc2642fc682f8dfafe83390b7
Egor Kharitonov <ega.khar@gmail.com>
2022-05-29 00:59:14 -0400

    basic junction

--- rockpapersci.py
+++ rockpapersci.py
@@ -2,4 +2,11 @@ import random
 
 list_moves = ["rock","paper","scissors"]
 
-computer_choise = random.choice(list_moves)
+computer_choice = random.choice(list_moves)
+
+human_choice = input("Please input rock, paper or scissors: ")
+
+if computer_choice == human_choice:
+    print("You have a tie!")
+else:
+    
